1fef0a94495177357163b95235318b4ff2fc83d9
Luke Przekwas <luke@covalence.io>
2018-09-05 21:20:31 -0500

    wiring up react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import HomeScreen from './screens/HomeScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 class App extends Component {
     render() {
-        return(
+        return (
             <Fragment>
-                <h1>Hello World</h1>
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={HomeScreen} />
+                        <Route path="/register" component={RegisterScreen} />
+                    </Switch>
+                </Router>
             </Fragment>
         );
     }
