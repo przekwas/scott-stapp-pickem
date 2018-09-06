9c830e1874c1dd13abfe92f7f542cb12371de57c
Luke Przekwas <luke@covalence.io>
2018-09-05 21:38:36 -0500

    adjusting imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import HomeScreen from './screens/HomeScreen';
-import RegisterScreen from './screens/RegisterScreen';
+import Navbar from './components/Navbar';
+import {
+    HomeScreen,
+    RegisterScreen,
+    LoginScreen,
+    ProfileScreen,
+    PickemScreen,
+    StandingsScreen
+} from './screens';
+
 
 class App extends Component {
     render() {
         return (
-            <Fragment>
-                <Router>
+            <Router>
+                <Fragment>
+
+                    <Navbar />
+
                     <Switch>
-                        <Route exact path="/" component={HomeScreen} />
-                        <Route path="/register" component={RegisterScreen} />
+                        <Route exact path='/' component={HomeScreen} />
+                        <Route path='/register' component={RegisterScreen} />
+                        <Route path='/login' component={LoginScreen} />
+                        {/* will make this a private route later */}
+                        <Route path='/profile' component={ProfileScreen} />
+                        <Route path='/pickem' component={PickemScreen} />
+                        <Route path='/standings' component={StandingsScreen} />
                     </Switch>
-                </Router>
-            </Fragment>
+
+                </Fragment>
+            </Router>
         );
     }
 };
