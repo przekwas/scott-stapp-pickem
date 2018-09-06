1784cbbf06cc138d1d748febbbc3c0a38860edcc
Luke Przekwas <luke@covalence.io>
2018-09-05 21:20:47 -0500

    adjusting directory architecture

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from 'react';
+
+class Home extends Component {
+    render() {
+        return(
+            <h1>Hello from Home Screen!</h1>
+        );
+    }
+};
+
+export default Home;
---
+++ src/screens/RegisterScreen.jsx
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from 'react';
+
+class RegisterScreen extends Component {
+    render() {
+        return(
+            <h1>Hello from Register Screen!</h1>
+        );
+    }
+};
+
+export default RegisterScreen;
