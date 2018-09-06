b530ce8ae622ab81b2fa92496c76a78577df2fc8
Luke Przekwas <luke@covalence.io>
2018-09-05 21:11:03 -0500

    testing git

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ class App extends Component {
     render() {
         return(
             <Fragment>
-                <h1>Hello World!</h1>
+                <h1>Hello World</h1>
             </Fragment>
         );
     }
