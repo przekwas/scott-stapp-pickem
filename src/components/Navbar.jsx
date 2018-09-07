20f38ffe52908aff3fc9e239cc5b25ccdd0e7cba
przekwas <luke@covalence.io>
2018-09-06 20:05:31 -0500

    beginning to style navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-const Navbar = () => {
-    return(
-        <div>
-            <Link to='/'>Home</Link>
-            <Link to='/register'>Register</Link>
-            <Link to='/login'>Login</Link>
-            <Link to='/profile'>Profile</Link>
-            <Link to='/pickem'>Pickem</Link>
-            <Link to='/standings'>Standings</Link>
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+    flex: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+};
+
+const Navbar = (props) => {
+    const { classes } = props;
+    return (
+        <div className={classes.root}>
+            <AppBar position='static'>
+                <Toolbar>
+                    <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="title" color="inherit" className={classes.flex}>
+                        Scott Stapp Memorial Pick'em League
+                    </Typography>
+                    <Button color="white"><Link to='/'>Home</Link></Button>
+                    <Link to='/register'>Register</Link>
+                    <Link to='/login'>Login</Link>
+                    <Link to='/profile'>Profile</Link>
+                    <Link to='/pickem'>Pickem</Link>
+                    <Link to='/standings'>Standings</Link>
+                </Toolbar>
+            </AppBar>
         </div>
     );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Navbar);
