1b3397fffed19d321aa969f4b44ba386b0f61b0d
Luke Przekwas <luke@covalence.io>
2018-09-14 13:29:40 -0500

    experimenting with creed logo

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,11 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import CreedLogo from '../assets/creed-logo.png';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
-class HomeScreen extends Component {
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        marginTop: theme.spacing.unit * 3
+    },
+});
+
+class PureHomeScreen extends Component {
+
     render() {
+        const { classes } = this.props;
         return (
-            <h1>Hello from Home Screen!</h1>
+            <Fragment>
+                <div className={classes.root}>
+                    <Grid container direction="row" justify="center" alignItems="center">
+                        <Grid item xs>
+                            <img alt='Creed Logo' src={CreedLogo} />
+                        </Grid>
+                    </Grid>
+                </div>
+            </Fragment>
         );
     }
 };
+
+PureHomeScreen.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+const HomeScreen = withStyles(styles)(PureHomeScreen);
 
 export { HomeScreen };
