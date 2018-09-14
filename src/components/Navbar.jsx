3a38655880bb43c44ddfd4cd9d20ba6c09201d1b
Luke Przekwas <luke@covalence.io>
2018-09-14 12:47:18 -0500

    adjusting navbar to use new logo font

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -16,6 +17,9 @@ const styles = theme => ({
     flex: {
         flexGrow: 1,
     },
+    appBar: {
+        backgroundColor: 'black',
+    },
     menuButton: {
         marginLeft: -12,
         marginRight: 20,
@@ -25,18 +29,21 @@ const styles = theme => ({
             backgroundColor: '#6573c3'
         }
     },
+    test: {
+        fontFamily: 'Metal Mania'
+    }
 });
 
 const PureNavbar = (props) => {
     const { classes } = props;
     return (
         <div className={classes.root}>
-            <AppBar position='static'>
+            <AppBar className={classes.appBar} position='static'>
                 <Toolbar>
                     <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
                         <MenuIcon />
                     </IconButton>
-                    <Typography variant="title" color="inherit" className={classes.flex}>
+                    <Typography variant="title" color="inherit" className={classNames(classes.flex, classes.test)}>
                         Scott Stapp Memorial Pick'em League
                     </Typography>
                     <Button color='inherit' className={classes.buttonHover} component={Link} to='/'>Home</Button>
