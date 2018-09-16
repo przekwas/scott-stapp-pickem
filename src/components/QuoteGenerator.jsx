8f61485943cd03a89b77ffb89d2e3b81f6b831cb
przekwas <luke@covalence.io>
2018-09-16 12:50:20 -0500

    starting quote gen

---
+++ src/components/QuoteGenerator.jsx
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from 'react';
+
+class QuoteGenerator extends Component {
+    render() {
+        return (
+            <p>Test!?</p>
+        );
+    }
+};
+
+export { QuoteGenerator }
