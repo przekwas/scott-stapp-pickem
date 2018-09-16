6de301b9788cc0ce16bf2adbff0f5ff933c32237
przekwas <luke@covalence.io>
2018-09-16 13:08:27 -0500

    wiring up quote gen component

--- src/components/QuoteGenerator.jsx
+++ src/components/QuoteGenerator.jsx
@@ -1,9 +1,28 @@
 import React, { Component, Fragment } from 'react';
+import mockQuoteGenerator from '../utilities/mockQuoteGenerator';
 
 class QuoteGenerator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quote: ''
+        };
+    }
+
+    async componentWillMount() {
+        try {
+            let quote = await mockQuoteGenerator();
+            this.setState({ quote });
+        } catch (error) {
+            console.log(`Failed to Generate/Fetch Random Quote: ${error}`);
+        }
+    }
+
     render() {
         return (
-            <p>Test!?</p>
+            <Fragment>
+                <p>" {this.state.quote} "</p>
+            </Fragment>
         );
     }
 };
