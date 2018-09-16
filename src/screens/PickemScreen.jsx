4ac260572c6baf1f94514efb24dfe2632b276b03
przekwas <luke@covalence.io>
2018-09-16 13:22:47 -0500

    testing paper component on pick em screen

--- src/screens/PickemScreen.jsx
+++ src/screens/PickemScreen.jsx
@@ -1,11 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
-class PickemScreen extends Component {
+const styles = theme => ({
+    root: {
+        ...theme.mixins.gutters(),
+        paddingTop: theme.spacing.unit * 2,
+        paddingBottom: theme.spacing.unit * 2,
+        margin: '1em'
+    },
+});
+
+class PurePickemScreen extends Component {
+
+
     render() {
+        const { classes } = this.props;
         return (
-            <h1>Hello from Pickem Screen!</h1>
+            <Fragment>
+                <Paper className={classes.root} elevation={1}>
+                    <Typography variant="headline" component="h3">
+                        This is a sheet of paper.
+                    </Typography>
+                    <Typography component="p">
+                        Paper can be used to build surface or other elements for your application.
+                    </Typography>
+                </Paper>
+            </Fragment>
         );
     }
 };
+
+PurePickemScreen.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+const PickemScreen = withStyles(styles)(PurePickemScreen);
 
 export { PickemScreen };
