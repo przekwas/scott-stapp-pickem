317ba09a3d9edc8a764ce70c0f8ae4c2aecb252b
przekwas <luke@covalence.io>
2018-09-16 13:40:03 -0500

    bringing in test data and team cards

--- src/screens/PickemScreen.jsx
+++ src/screens/PickemScreen.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import mockTeamGenerator from '../utilities/mockTeamGenerator';
+import { TeamCard } from '../components/TeamCard';
 
 const styles = theme => ({
     root: {
@@ -14,19 +16,33 @@ const styles = theme => ({
 });
 
 class PurePickemScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            homeTeam: {},
+            awayTeam: {}
+        };
+    }
 
+    async componentWillMount() {
+        try {
+            let homeTeam = await mockTeamGenerator();
+            let awayTeam = await mockTeamGenerator();
+            this.setState({
+                homeTeam,
+                awayTeam
+            });
+        } catch (error) {
+            console.log(`Error Fetching Team: ${error}`);
+        }
+    }
 
     render() {
         const { classes } = this.props;
         return (
             <Fragment>
                 <Paper className={classes.root} elevation={1}>
-                    <Typography variant="headline" component="h3">
-                        This is a sheet of paper.
-                    </Typography>
-                    <Typography component="p">
-                        Paper can be used to build surface or other elements for your application.
-                    </Typography>
+                    <TeamCard />
                 </Paper>
             </Fragment>
         );
