ab9df5b870a9f635c9d4cca2e9e9cb9854f63ab4
przekwas <luke@covalence.io>
2021-09-21 22:13:11 -0500

    adjusting client uri for quick dev

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-    uri: 'https://lukes-projects.herokuapp.com/graphql',
+    uri: process.env.REACT_APP_QUERY_URL || 'https://lukes-projects.herokuapp.com/graphql',
     cache: new InMemoryCache()
 });
